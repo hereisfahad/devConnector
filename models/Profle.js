7306a95a117a97e38a598c8555fbe1a8bfc80d2c
hereisfahad <fahadjabbar31@gmail.com>
2019-10-19 22:24:03 +0500

    updated profile route to get current user profile

--- models/Profle.js
+++ models/Profle.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
+  //mongoose.Schema wasnt working
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'users'
+    ref: "users"
   },
   handle: {
     type: String,
@@ -117,4 +118,4 @@ const ProfileSchema = new Schema({
   }
 });
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,24 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-router.get('/',(req,res) =>{
-    res.send('profile route');
-})
+const Profile = require("../../models/Profle");
+const User = require("../../models/User");
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(404).json({ msg: "No profile for this user" });
+    }
+    return res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json("server error");
+  }
+  //   res.send("profile route");
+});
 module.exports = router;
